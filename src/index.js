eb730c4adae31aabe725a8d2f98cf15177403f34
piotrchmielowiec.tbg.70 <piotrchmielowiec.tbg.70@gmail.com>
2021-10-19 12:46:07 +0200

    Write some important comments about further code development

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 /**
- ///// *TODO: place checking right after the square is dropped!!! 
+ * TODO: refactor functions responsible for checking rows an collumns
  * *Features to be added:
  * *STYLLING!!!
- * *Checking if possible move exists
- * *Disallowing if move to be done is not valid
  * *Add web techologies logos to square
  */
+
 const grid = document.querySelector(".grid");
 const width = 8;
 const squares = [];
@@ -25,7 +24,10 @@ let squareIdBeingDragged;
 let colorBeingReplaced;
 let squareIdBeingReplaced;
 
-// Create board
+/**
+ * * Checking after creating a board is done in order to ensure that there are no 
+ * * valid rows or collumns
+ */
 function createBoard(){
   for (let i = 0; i <width*width; i++) {
     const square = document.createElement("div");
@@ -45,6 +47,10 @@ checkColumnForFour();
 checkRowForThree();
 checkColumnForThree(); 
 
+
+/**
+ * Initialize drag API
+ */
 squares.forEach(square => square.addEventListener("dragstart", dragStart)); 
 squares.forEach(square => square.addEventListener("dragend", dragEnd)); 
 squares.forEach(square => square.addEventListener("dragover", dragOver)); 
@@ -58,16 +64,17 @@ function dragStart(){
 }
 
 /**
- * /////! Drag does NOT work while interacting with first square
- * /////! both form left to right and bottom to up
  * ? Also sometimes diagonal moves are possible
  * ? don't really know why and how to replicate them consistently
  * ? update to above comment: didn't find any issiues lately :thinking:
  * TODO: Deal with above bugs
- * * Checking for four works as intended
- * ? Maybe i need to rewrite functions and make everything in only 2 functions (rows and cols)?
  */
 
+/**
+ * * dragEnd prevents user from moving differently than just one left, right, up or down.
+ * * If yes squareIdBeingReplaced is nulled so it is ready for next call, then
+ * * dragDrop is called.
+ */
 function dragEnd(){
   let validMoves = [squareIdBeingDragged - 1,
     squareIdBeingDragged - width,
@@ -87,43 +94,20 @@ function dragEnd(){
   checkColumnForThree();
 }
 
-function checkRowThree(){
-  for (let i = 0; i <= 61; i++){
-    let rowOfThree = [i, i + 1, i + 2];
-    let decidedColor = squares[i].style.backgroundColor;
-    const isBlank = squares[i].style.backgroundColor === "";
-
-    const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
-    if (notValid.includes(i)) continue;
-    if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank )){
-      return true;
-    }
-  }
-  return false;
-}
 /**
- /////* ! Last row is not taken into consideration thus chceckColumnForThree is not working as expected
+ * * Correct if statement below because it looks funny.
+ * * This function also check if move that is to be done results in breaking block of candies
+ * * Firstly it perform move, then checks if block is able to break. If yes, block is broken
+ * * and game is ready to continue. If not, move is reversed which blocks player from making such move.
  */
-function checkColumnThree() {
-  for (i = 0; i < 48; i ++) {
-    let columnOfThree = [i, i+width, i+width*2]
-    let decidedColor = squares[i].style.backgroundColor
-    const isBlank = squares[i].style.backgroundColor === ''
-
-    if(columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
-      return true;
-    }
-  }
-  return false;
-}
-
 function dragDrop(){
   colorBeingReplaced = this.style.backgroundColor; 
   squareIdBeingReplaced = parseInt(this.id);
   squares[squareIdBeingDragged].style.backgroundColor = colorBeingReplaced;
   this.style.backgroundColor = colorBeingDragged;
+
   if(checkRowThree() || checkColumnThree()){
-  }else{
+  }else {
     this.style.backgroundColor = colorBeingReplaced;
     squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
   }
@@ -134,7 +118,11 @@ function dragEnter(e){ e.preventDefault(); }
 function dragLeave(){}
 
 /**
- * * drop candies once some have been cleared
+ * * Dropping candies is performed once in 150 ms. One call of moveDown 
+ * * moves the candies one row down. If there is no room to move 
+ * * function just does nothing apart from checking if there are any breakable blocks.
+ * * Second if is responsible for generating new candies if there is the space for them.
+ * * If yes then blocks are broken and score is updated. 
  */
 function moveDown(){
   for(let i = 0; i < 56; i++){
@@ -155,14 +143,49 @@ function moveDown(){
     checkRowForThree();
     checkColumnForThree();
     scoreHolder.innerHTML = "Score: " + score;
-
+    
   }
 }
 /**
-  * * Checking for matches
-  * // Function chcecks for match even where row breaks to next row
-  * //TODO: Repair checking 
-**/
+ * * Checking for matches is based upon the element background color
+ * * It is fine but consider using "data" html field.
+ * TODO: strong refactor
+ * * Code below needs some strong refactoring. Too much functions. Redce them to max 2 functions.
+ * * One to check for rows and one for collumns.
+ * * Remember that functions checkRowThree and checkColumnThree are used to chech if move 
+ * * resolves in breaking block of candies. 
+ * * Other functions are just for checking if such blocks exists.
+ * * Primarly i did it because those functions were not returning anything and i needed bolean value.
+ * * notValid array is there to ensure that no blocks are valid if block breaks in another row.
+ * // Function chcecks for match even where row breaks to next row
+ * //TODO: Repair checking 
+ **/
+function checkRowThree(){
+  for (let i = 0; i <= 61; i++){
+    let rowOfThree = [i, i + 1, i + 2];
+    let decidedColor = squares[i].style.backgroundColor;
+    const isBlank = squares[i].style.backgroundColor === "";
+
+    const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
+    if (notValid.includes(i)) continue;
+    if (rowOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank )){
+      return true;
+    }
+  }
+  return false;
+}
+function checkColumnThree() {
+  for (i = 0; i < 48; i ++) {
+    let columnOfThree = [i, i+width, i+width*2]
+    let decidedColor = squares[i].style.backgroundColor
+    const isBlank = squares[i].style.backgroundColor === ''
+
+    if(columnOfThree.every(index => squares[index].style.backgroundColor === decidedColor && !isBlank)) {
+      return true;
+    }
+  }
+  return false;
+}
 function checkRowForFour(){
   for (let i = 0; i <= 60; i++){
     let rowOfFour = [i, i + 1, i + 2, i + 3];
@@ -210,9 +233,6 @@ function checkRowForThree(){
     }
   }
 }
-/**
- /////* ! Last row is not taken into consideration thus chceckColumnForThree is not working as expected
- */
 function checkColumnForThree() {
   for (i = 0; i < 48; i ++) {
     let columnOfThree = [i, i+width, i+width*2]
